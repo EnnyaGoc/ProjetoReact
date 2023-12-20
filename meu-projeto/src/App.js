30a27cd4320176e1d265a360898e865f13c6f11b
Ênnya Gomes <ennyagomesoc@gmail.com>
2023-12-20 00:34:32 -0300

    inicio

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -2,10 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const name = 'ennya'
+
+  const newName = name.toUpperCase()
+
+  function sum(a, b){
+    return a+b
+  }
+
+  const url = "https://via.placeholder.com/150"
   return (
     <div className="App">
      <h1>Olá react!</h1>
-     <p> meu primeiro app</p>
+     <p> olá, {newName}</p>
+     <p>Soma: {sum(1, 2)}</p>
+     <img src={url} alt="imagem"></img>
     </div>
   );
 }
