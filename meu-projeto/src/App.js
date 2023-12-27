f1821294eef21595bd6ed91933459b1b308bae50
Ênnya Gomes <ennyagomesoc@gmail.com>
2023-12-27 00:16:57 -0300

    continuando

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
+import Frase from './components/Frase';
+import List from './components/List';
 
 function App() {
 
 const nome = "maria"
   return (
     <div className="App">
-     
-     
+     <h1>Testando css</h1>
+     <Frase />
+     <Frase />
+     <Frase />
      <SayMyName nome = "ennya"/>
      <SayMyName nome = "julia"/>
      <SayMyName nome={nome} />
@@ -18,6 +22,7 @@ const nome = "maria"
         profissao="programadora"
         foto="https://"
      />
+     <List></List>
     </div>
   );
 }
--- meu-projeto/src/components/Frase.js
+++ meu-projeto/src/components/Frase.js
@@ -1,7 +1,9 @@
+import styles from './Frase.module.css'
+
 function Frase(){
 return (
-    <div>
-        <p>componente com uma frase</p>
+    <div className={styles.fraseContainer}>
+        <p className={styles.fraseContent}>componente com uma frase</p>
     </div>
 )
 }
