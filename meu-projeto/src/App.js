99ace1c9cb145d0f831e73d83108fe985e756be7
Ênnya Gomes <ennyagomesoc@gmail.com>
2023-12-29 23:56:37 -0300

    .

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,28 +1,16 @@
 import './App.css';
-import SayMyName from './components/SayMyName';
-import Pessoa from './components/Pessoa';
-import Frase from './components/Frase';
-import List from './components/List';
+import Evento from './components/Evento';
+import Form from './components/Form';
 
 function App() {
 
 const nome = "maria"
   return (
     <div className="App">
-     <h1>Testando css</h1>
-     <Frase />
-     <Frase />
-     <Frase />
-     <SayMyName nome = "ennya"/>
-     <SayMyName nome = "julia"/>
-     <SayMyName nome={nome} />
-     <Pessoa 
-        nome="ennya"
-        idade="21"
-        profissao="programadora"
-        foto="https://"
-     />
-     <List></List>
+     <h1>Testando Eventos</h1>
+     <Evento numero="1"/>
+     <Evento numero="2"/>
+     <Form />
     </div>
   );
 }
---
+++ meu-projeto/src/components/Form.js
@@ -0,0 +1,22 @@
+function Form(){
+
+    function cadastrarUsuario(e){
+        e.preventDefault()
+        console.log("Cadastrou usuario")
+    }
+    return(
+        <div>
+            <h1>Meu cadastro:</h1>
+            <form onSubmit={cadastrarUsuario}>
+                <div>
+                    <input type="text" placeholder="Digite seu nome" />
+                </div>
+                <div>
+                    <input type="submit" placeholder="Cadastrar" />
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default Form
