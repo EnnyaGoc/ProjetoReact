d4ce72c803cbe03fcc2c54ab52be4b2fcf1eb249
Ênnya Gomes <ennyagomesoc@gmail.com>
2024-01-14 21:41:53 -0300

    .

--- meu-projeto/src/components/layout/Footer.js
+++ meu-projeto/src/components/layout/Footer.js
@@ -1,8 +1,20 @@
-import { FaFacebook } from 'react-icons/fa'
+import { FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa'
+import styles from './Footer.module.css'
 
 function Footer(){
-    return <Footer>
-        <FaFacebook />
-    </Footer>
+    return <footer>
+        <ul className={styles.social_list}>
+            <li>
+                <FaFacebook />
+            </li>
+            <li>
+                <FaInstagram />
+            </li>
+            <li>
+                <FaLinkedin />
+            </li>
+        </ul>
+        <p>Nosso Rodapé</p>
+    </footer>
 }
 export default Footer
